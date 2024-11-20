587336ed52badc4fab194d4f032fe75dd9b20050
costa <afonsovelosof@gmail.com>
2024-11-20 14:25:53 +0000

    Update Header component: replace title with a link to the home page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,15 @@
-import { ModeToggle } from "../../ui/toggle-mode";
+import { ModeToggle } from "@/components/ui/toggle-mode";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <>
             <header className="flex items-center justify-between p-4 border-b-2 border-text-foreground mb-4">
-                <h1 className="text-2xl font-bold">Weather App</h1>
+                <Link
+                    href="/"
+                    className="text-2xl font-bold">
+                        Weather App
+                </Link>
 
                 <ModeToggle />
             </header>
