48d8e1230517eb637d1c30fc3c559bdb517cf489
costa <afonsovelosof@gmail.com>
2024-11-24 15:06:58 +0000

    Enhance header styling for improved visibility in dark mode

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 export default function Header() {
     return (
         <>
-            <header className="flex items-center justify-between p-4 border-b border-zinc-900 mb-4">
+            <header className=" flex items-center justify-between p-4 border-b border-zinc-900/20 dark:border-zinc-300/20 mb-4">
                 <Link
                     href="/"
                     className="text-2xl font-bold">
-                        Weather App
+                    Weather App
                 </Link>
 
                 <ModeToggle />
