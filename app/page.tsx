d07ce1fe4022a697bdd520374f716ddeae9da1e8
Costa <afonsovelosof@gmail.com>
2024-11-24 23:36:51 +0000

    Add WeatherCard component to Home page for displaying weather information

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import WeatherFrom from '@/app/components/pages/weather/WeatherForm';
+import WeatherCard from '@/app/components/ui/WeatherCard';
 
 export default function Home() {
   return (
@@ -6,6 +7,9 @@ export default function Home() {
       <main className="min-h-full flex items-center justify-center p-4 sm:p-6 md:p-8 lg:p-10">
         <div className="w-full max-w-md">
           <WeatherFrom />
+          <div>
+            <WeatherCard />
+          </div>
         </div>
       </main>
 
