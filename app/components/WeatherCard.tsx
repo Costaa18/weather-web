2e5cbfd3b3b399b1cc6959a24238d2a0b4aede14
costa <afonsovelosof@gmail.com>
2024-11-24 15:07:04 +0000

    Add loading state to WeatherCard component and improve weather data display

--- app/components/WeatherCard.tsx
+++ app/components/WeatherCard.tsx
@@ -27,6 +27,15 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ city }) => {
     const today = new Date();
     const date = today.toLocaleDateString(userLanguage, { weekday: 'long', month: '2-digit', day: 'numeric' });
 
+    if (loading) {
+        return (
+            <div className='flex justify-center items-center'>
+                <div className="custom-loader"></div>
+            </div>
+
+        )
+    }
+
     const metersToKm = (meters: number) => {
         return meters / 1000;
     };
@@ -38,17 +47,7 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ city }) => {
     return (
         <div className="m-10 items-center flex flex-col md:flex-row md:justify-center">
             <div className="w-72 flex flex-col bg-card dark:bg-primary p-4 rounded-lg transition duration-500 ease-in-out transform hover:scale-105 cursor-pointer shadow-xl shadow-foregorund/60 dark:shadow-primary/40">
-                {loading ? (
-                    <div className="flex flex-col justify-center items-center h-48">
-                        <svg className="pl" width="240" height="240" viewBox="0 0 240 240">
-                            <circle className="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" strokeWidth="20" strokeDasharray="0 660" strokeDashoffset="-330" strokeLinecap="round"></circle>
-                            <circle className="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" strokeWidth="20" strokeDasharray="0 220" strokeDashoffset="-110" strokeLinecap="round"></circle>
-                            <circle className="pl__ring pl__ring--c" cx="85" cy="120" r="70" fill="none" stroke="#000" strokeWidth="20" strokeDasharray="0 440" strokeLinecap="round"></circle>
-                            <circle className="pl__ring pl__ring--d" cx="155" cy="120" r="70" fill="none" stroke="#000" strokeWidth="20" strokeDasharray="0 440" strokeLinecap="round"></circle>
-                        </svg>
-                        <span className='text-sm'>Getting Weather Data</span>
-                    </div>
-                ) : weatherData ? (
+                {weatherData ? (
                     <>
                         <div className="font-bold text-xl uppercase">{weatherData.city}, {weatherData.country}</div>
                         <div className="text-sm">{date}</div>
@@ -63,14 +62,14 @@ const WeatherCard: React.FC<WeatherCardProps> = ({ city }) => {
                         <div className="flex flex-row items-center justify-center mt-6">
                             <div className="font-medium text-6xl">{roundToUnits(weatherData.temperature)}°</div>
                             <div className="flex flex-col items-center ml-6">
-                                <div>Cloudy</div>
+                                <div>{weatherData.weather}</div>
                                 <div className="mt-1">
                                     <span className="text-sm"><i className="far fa-long-arrow-up"></i></span>
-                                    <span className="text-sm font-light text-gray-500">{roundToUnits(weatherData.tempMax)}°C</span>
+                                    <span className="text-sm font-light">{roundToUnits(weatherData.tempMax)}°C</span>
                                 </div>
                                 <div>
                                     <span className="text-sm"><i className="far fa-long-arrow-down"></i></span>
-                                    <span className="text-sm font-light text-gray-500">{roundToUnits(weatherData.tempMin)}°C</span>
+                                    <span className="text-sm font-light">{roundToUnits(weatherData.tempMin)}°C</span>
                                 </div>
                             </div>
                         </div>
