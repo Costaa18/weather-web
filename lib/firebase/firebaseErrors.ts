7b79fb0a20c63cb04abfd1c934d3c8ecbdf21aea
Costa <afonsovelosof@gmail.com>
2024-11-25 12:01:08 +0000

    Add firebaseErrors module for handling Firebase authentication error messages

---
+++ lib/firebase/firebaseErrors.ts
@@ -0,0 +1,11 @@
+const firebaseErrors = {
+    "auth/email-already-exists": "O email já esta em uso.",
+    "auth/invalid-email": "O email é inválido.",
+    "auth/weak-password": "A palavra-passe é fraca.",
+    "auth/user-not-found": "Utilizador não encontrado.",
+    "auth/wrong-password": "Palavra-Passe incorreta.",
+    "auth/account-exists-with-different-credential": "Já existe uma conta com o mesmo email.",
+};
+
+
+export default firebaseErrors;
