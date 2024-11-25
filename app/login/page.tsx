aa57255397be4f7d0d0f3f3f3002b76535ea8a56
Costa <afonsovelosof@gmail.com>
2024-11-25 12:00:53 +0000

    Add LoginPage component to render the LoginForm for user authentication

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/app/components/pages/auth/LoginForm";
+
+export default function LoginPage() {
+    return (
+        <div className="flex h-[75vh] w-full items-center justify-center px-4">
+            <LoginForm />
+        </div>
+    )
+}
