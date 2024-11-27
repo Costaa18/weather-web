e5c185911a9eabfffc68745f999612fd29612dcd
Costa <afonsovelosof@gmail.com>
2024-11-27 15:23:29 +0000

    Add function to create user and fetch city by name from API

---
+++ api/weather/create-user.ts
@@ -0,0 +1,21 @@
+import api from '@/lib/axios';
+
+interface UserData {
+    uid: string;
+    displayName: string;
+    email: string;
+    photoURL: string;
+    provider: string;
+    emailVerified: boolean;
+}
+
+export async function getCityByName(user: UserData): Promise<UserData | undefined> {
+    try {
+        const apiResponse = await api.post<UserData>(`${process.env.NEXT_PUBLIC_API_URL}/users`,);
+        apiResponse.data;
+        return apiResponse.data;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return undefined;
+    }
+}
