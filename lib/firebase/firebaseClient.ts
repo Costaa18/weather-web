f700952e1dcef2d1b0061fc366cd3834d0455304
Costa <afonsovelosof@gmail.com>
2024-11-27 15:25:56 +0000

    Ensure single instance of Firebase app initialization and export auth for project use

--- lib/firebase/firebaseClient.ts
+++ lib/firebase/firebaseClient.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// /lib/firebase/firebaseClient.js
+import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,10 +9,11 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+// Certifique-se de inicializar apenas uma instância do app
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+
+// Exporta o auth para uso em outras partes do projeto
 export const auth = getAuth(firebaseApp);
-//const analytics = getAnalytics(firebaseApp);
