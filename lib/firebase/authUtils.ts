e5cffc75850cc6f73c14f4601f5931b1f6509112
Costa <afonsovelosof@gmail.com>
2024-11-27 15:25:52 +0000

    Add persistence for Google login and remove session cookie on logout

--- lib/firebase/authUtils.ts
+++ lib/firebase/authUtils.ts
@@ -1,34 +1,37 @@
 // lib/firebase/authUtils.ts
 import { GoogleAuthProvider, GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import { signOut } from "firebase/auth";
+import { setPersistence, browserLocalPersistence } from "firebase/auth";
 import { auth } from "./firebaseClient";
+import { removeCookie } from "../cookies";
 
-// Login com Google
 export const loginWithGoogle = async (): Promise<string> => {
   const provider = new GoogleAuthProvider();
   try {
+    await setPersistence(auth, browserLocalPersistence);
     const result = await signInWithPopup(auth, provider);
     const user = result.user;
+    console.log("Usuário logado com Google:", user);
     const token = await user.getIdToken();
-    console.log("Usuário Google logado:", user);
-    return token; // Retorna o token JWT
+    return token;
   } catch (error) {
-    console.error("Erro ao fazer login com Google:", error);
+    console.error("Erro no login com Google:", error);
     throw error;
   }
 };
+
 
 // Login com GitHub
 export const loginWithGithub = async (): Promise<string> => {
   const provider = new GithubAuthProvider();
   try {
     const result = await signInWithPopup(auth, provider);
+
     const user = result.user;
     const token = await user.getIdToken();
-    console.log("Usuário GitHub logado:", user);
-    return token; // Retorna o token JWT
+
+    return token;
   } catch (error) {
-    console.error("Erro ao fazer login com GitHub:", error);
     throw error;
   }
 };
@@ -36,8 +39,8 @@ export const loginWithGithub = async (): Promise<string> => {
 export const logout = async (): Promise<void> => {
   try {
     await signOut(auth);
-    console.log("Usuário deslogado.");
+    removeCookie("session");
   } catch (error) {
-    console.error("Erro ao fazer logout:", error);
+    throw error;
   }
 };
